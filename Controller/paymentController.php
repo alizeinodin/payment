bf075f0f652204f58b7351e7f5332dfd35b42a22
Ali Zeinodin <alizeinodin79@gmail.com>
2022-12-27 19:16:49 +0330

    paymentController done

--- Controller/paymentController.php
+++ Controller/paymentController.php
@@ -1,9 +1,18 @@
 <?php
 require_once 'Controller.php';
+require_once '../Model/database.php';
 
-class payment
+class payment implements Controller
 {
     private $callback_uri = "https://ssces.barfenow.ir/Controller/paymentController.php";
+
+    private $trans_id;
+    private $order_id;
+    private $amount;
+
+    private $res;
+
+
     public function tokenRequest($orderId)
     {
         $curl = curl_init();
@@ -35,8 +44,90 @@ class payment
         header("location:https://nextpay.org/nx/gateway/payment/{$token}");
     }
 
-    public function ()
+    public function validation()
     {
+        if($this->res['code'] != 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public function prepare()
+    {
+        $this->trans_id = $_GET['trans_id'];
+        $this->order_id = $_GET['order_id'];
+        $this->amount = $_GET['amount'];
+
+        $this->updateTransOfPay();
+
+        $this->acceptTrans();
+
+        if (!$this->validation()) {
+            die("پرداخت ناموفق بود. لطفا دوباره تلاش کنید!");
+        }
+
+        $_DB = new DB();
+
+        $prepare = $_DB->pdo->prepare("UPDATE `pay` set card_holder = '{$this->res['card_holder']}',
+                 customer_phone = '{$this->res['customer_phone']}',
+                 Shaparak_Ref_Id = '{$this->res['Shaparak_Ref_Id']}',
+                 trans_created_at = '{$this->res['created_at']}',
+                 status = 'accepted' WHERE id = '{$this->order_id}'");
+        $prepare->execute();
+
+        $prepare = $_DB->pdo->prepare("SELECT name, stn FROM `user`, `pay` WHERE pay.id = '{$this->order_id}' and pay.user_id = user.id");
+        $prepare->execute();
+        $result = $prepare->fetchAll();
+
+        $user = $result[0];
+
+        $_SESSION['name'] = $user['name'];
+        $_SESSION['stn'] = $user['stn'];
+
+        header("location:https://ssces.barfenow.ir/ticket.php");
+
+
+
+    }
+
+    public function acceptTrans()
+    {
+        $_DB = new DB();
+
+        $prepare = $_DB->pdo->prepare("SELECT * FROM `pay` WHERE id = {$this->order_id} and trans_id = {$this->trans_id}");
+        $prepare->execute();
+        $res = $prepare->fetchAll();
+
+        $record = $res[0];
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://nextpay.org/nx/gateway/verify',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => "api_key=65d94dfb-19d8-4357-bcf4-cf570abcf251&amount={$record['amount']}&trans_id={$record['trans_id']}",
+        ));
+
+        $response = json_decode(curl_exec($curl));
+        curl_close($curl);
+
+        $this->res = $response;
+
+        return $response;
+    }
+
+    private function updateTransOfPay()
+    {
+        $_DB = new DB();
+
+        $prepare = $_DB->pdo->prepare("UPDATE `pay` set trans_id = '{$this->trans_id}' WHERE id = '{$this->order_id}'");
+        $prepare->execute();
 
     }
 }
