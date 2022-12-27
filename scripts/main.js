c7ddb3c18f2059ffcb9a1d1b265682f4f35eef71
asma <as.ch.2280@gmail.com>
2022-12-27 15:46:23 +0330

    add js file for show errors

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+let Name = document.getElementById("name");
+let nationcode = document.getElementById("nationcode");
+let stunumber = document.getElementById("stunumber");
+let tell = document.getElementById("tell");
+const showNameError = () => {
+  Name.style.display = "block";
+};
+const showNationCodeError = () => {
+  nationcode.style.display = "block";
+};
+const showStuNumberError = () => {
+  stunumber.style.display = "block";
+};
+const showTellError = () => {
+  tell.style.display = "block";
+};
