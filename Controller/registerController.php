ac5db0a6343f3ad7785a1d1a643ccc388705b69c
Ali Zeinodin <alizeinodin79@gmail.com>
2022-12-27 03:43:15 +0330

    registerController implements Controller

--- Controller/registerController.php
+++ Controller/registerController.php
@@ -1,3 +1,18 @@
 <?php
 require_once '../Model/database.php';
 
+class registerController implements Controller
+{
+    private $arr = [];
+
+    public function validation()
+    {
+
+    }
+
+    public function prepare()
+    {
+        $this->validation();
+    }
+}
+
